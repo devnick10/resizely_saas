d5427d2fefd29b31a5a58ce2fc3aa24e0cc624b2
devnick10 <nickbhoyar683@gmail.com>
2025-12-19 01:05:50 +0530

    chore: remove dead comments

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -1,11 +1,6 @@
 import Razorpay from "razorpay";
 import { NextResponse, NextRequest } from "next/server";
 
-// const razorpay = new Razorpay({
-//   key_id: process.env.RAZOR_PAY_KEY_ID as string,
-//   key_secret: process.env.RAZOR_KEY_SECRET as string,
-// });
-
 export async function POST(request: NextRequest) {
   try {
     const { amount } = (await request.json()) as {
