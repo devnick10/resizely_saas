be2464749d06c49b3a59e8789187290df3ff4adf
devnick10 <nickbhoyar683@gmail.com>
2025-12-26 19:13:02 +0530

    chore: remove store initialization from layout for fast layout loading

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -1,11 +1,5 @@
-import { Loader } from "@/components/core/Loader";
-import { Videos } from "@/components/dashboard/Videos";
-import { Suspense } from "react";
+import { Home } from "@/components/dashboard/pages/home";
 
 export default function Page() {
-  return (
-    <Suspense fallback={<Loader />}>
-      <Videos />
-    </Suspense>
-  );
+  return <Home />;
 }
--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,32 +1,25 @@
-import React from "react";
 import { Navbar } from "@/components/dashboard/Navbar";
 import { Sidebar } from "@/components/dashboard/Sidebar";
-import { getUser } from "../../lib/data/user/getUser";
-import { StoreInitializer } from "@/components/core/StoreInitializer";
-import { getCredits } from "@/lib/data/user/getCredits";
+import React from "react";
 
 export default async function AppLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const [user, credits] = await Promise.all([getUser(), getCredits()]);
   return (
-    <>
-      <StoreInitializer user={user} credits={credits} />
-      <div className="flex h-screen flex-col bg-white text-black transition-colors dark:bg-background dark:text-white">
-        <Navbar />
-        <div className="flex flex-1 overflow-hidden">
-          {/* Desktop sidebar */}
-          <aside className="hidden w-64 flex-col border-r border-gray-200 bg-white dark:border-gray-800 dark:bg-muted lg:flex">
-            <Sidebar />
-          </aside>
-          {/* Main content */}
-          <main className="w-full flex-1 overflow-auto">
-            <div className="h-full w-full p-0 dark:bg-background sm:p-4 md:p-6">
-              {children}
-            </div>
-          </main>
-        </div>
+    <div className="flex h-screen flex-col bg-white text-black transition-colors dark:bg-background dark:text-white">
+      <Navbar />
+      <div className="flex flex-1 overflow-hidden">
+        {/* Desktop sidebar */}
+        <aside className="hidden w-64 flex-col border-r border-gray-200 bg-white dark:border-gray-800 dark:bg-muted lg:flex">
+          <Sidebar />
+        </aside>
+        {/* Main content */}
+        <main className="w-full flex-1 overflow-auto">
+          <div className="h-full w-full p-0 dark:bg-background sm:p-4 md:p-6">
+            {children}
+          </div>
+        </main>
       </div>
-    </>
+    </div>
   );
 }
