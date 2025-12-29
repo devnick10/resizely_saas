f574c80daf3787fedd98cac7f8fedd7ef95067ab
devnick10 <nickbhoyar683@gmail.com>
2025-12-29 15:58:54 +0530

    chore: remove logs

--- src/helper/mailer.ts
+++ src/helper/mailer.ts
@@ -20,8 +20,8 @@ export const sendMail = async (email: string, otp: string) => {
 
     return mailresponse;
   } catch (error) {
-    console.log(error);
-    throw new Error(error as string);
+    console.error(error);
+    throw error;
   }
 };
 
